0c5ab267656d6bcba0013e729b06ddbbc3399f7d
Goran Cipar <gcipar@gmail.com>
2025-02-01 13:14:51 +0100

    Initial Commit

---
+++ elevator/elevatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  elevatorApp.swift
+//  elevator
+//
+//  Created by Goran Cipar on 01.02.25.
+//
+
+import SwiftUI
+
+@main
+struct elevatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ elevatorTests/elevatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  elevatorTests.swift
+//  elevatorTests
+//
+//  Created by Goran Cipar on 01.02.25.
+//
+
+import Testing
+@testable import elevator
+
+struct elevatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
